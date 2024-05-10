810841e1f20735d375cf943c4f6af49cec5891b4
ErickGichuki <151629292+ErickGichuki@users.noreply.github.com>
2024-05-10 10:29:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const jsonServer = require('json-server')
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
-const server = express()
-server.use('/products',jsonServer.router('db.json'))
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
